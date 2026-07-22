2fed213a451873012e4aa8750b6f5de64334c677
Andre Andrade <andre.felipe@tecnico.ufcg.edu.br>
2026-07-22 08:26:26 -0300

    update translations

--- locales/en_GB.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'Subdomains are also accepted. E.g.: "company.com" will allow user@company.com and user@sub.company.com.' => 'Subdomains are also accepted. E.g.: "company.com" will allow user@company.com and user@sub.company.com.',
-];
